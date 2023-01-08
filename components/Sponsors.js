21b3a724674941dbe92c8963f19d8e005b4868ed
Raj Patel <riskyrick20@gmail.com>
2023-01-08 15:19:28 +0530

    changed a sponser logo and navbar bug fixes

--- components/Sponsors.js
+++ components/Sponsors.js
@@ -23,15 +23,18 @@ export default function Sponsors() {
           borderWidth={"2px"}
           borderColor={"blue.400"}
           cursor={"pointer"}
+          display={"flex"}
+          justifyContent={"center"}
+          alignItems={"center"}
         >
-          <Image src="https://images.unsplash.com/photo-1516876437184-593fda40c7ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGxvZ298ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"></Image>
+          <Image src="/GMC_Logo.png" width={32} />
         </GridItem>
         <GridItem
           borderWidth={"2px"}
           borderColor={"blue.400"}
           cursor={"pointer"}
         >
-          <Image src="https://images.unsplash.com/photo-1516876437184-593fda40c7ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGxvZ298ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"></Image>
+          <Image src=""></Image>
         </GridItem>
         <GridItem
           borderWidth={"2px"}
