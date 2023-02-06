8260cff2a9cb64c6f907232fa7183490659c9a11
Raj Patel <riskyrick20@gmail.com>
2023-02-06 20:59:21 +0530

    added new sponsers

--- components/Partners.js
+++ components/Partners.js
@@ -47,15 +47,6 @@ export default function Partners() {
           <Image src="https://images.unsplash.com/photo-1516876437184-593fda40c7ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGxvZ298ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"></Image>
         </GridItem>
       </Grid>
-      <Flex justifyContent={"center"} marginBlock={8}>
-        <Button
-          bgColor={"blue.400"}
-          color={"white"}
-          _hover={{ bgColor: "blue.600" }}
-        >
-          Be a Partner
-        </Button>
-      </Flex>
     </Box>
   );
 }
--- components/Sponsors.js
+++ components/Sponsors.js
@@ -32,27 +32,34 @@ export default function Sponsors() {
           </Link>
         </GridItem>
 
-        <GridItem cursor={"pointer"}>
-          <Image src="https://images.unsplash.com/photo-1516876437184-593fda40c7ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGxvZ298ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"></Image>
+        <GridItem
+          cursor={"pointer"}
+          display={"flex"}
+          justifyContent={"center"}
+          alignItems={"center"}
+          height="48"
+        >
+          <Link href={"https://d360.tech/"} target="_blank">
+            <Image src="/d360.png" />
+          </Link>
         </GridItem>
 
-        <GridItem cursor={"pointer"}>
-          <Image src="https://images.unsplash.com/photo-1516876437184-593fda40c7ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGxvZ298ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"></Image>
+        <GridItem
+          cursor={"pointer"}
+          display={"flex"}
+          justifyContent={"center"}
+          alignItems={"center"}
+          height="48"
+        >
+          <Link href={"https://www.jklakshmicement.com/"} target="_blank">
+            <Image src="/jk.png" />
+          </Link>
         </GridItem>
 
         <GridItem cursor={"pointer"}>
           <Image src="https://images.unsplash.com/photo-1516876437184-593fda40c7ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGxvZ298ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"></Image>
         </GridItem>
       </Grid>
-      <Flex justifyContent={"center"} marginBlock={8}>
-        <Button
-          bgColor={"blue.400"}
-          color={"white"}
-          _hover={{ bgColor: "blue.600" }}
-        >
-          Sponsor Us
-        </Button>
-      </Flex>
     </Box>
   );
 }
